d329e05d76acd9600972627f59e22c81752bd783
Holices Junior <dev.holices@gmail.com>
2025-01-30 11:44:50 +0000

    perf: change staleTime

--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -16,7 +16,7 @@ export function Summary() {
   const { data } = useQuery({
     queryKey: ['summary'],
     queryFn: getSummary,
-    staleTime: 1000 * 60,
+    staleTime: 1000 * 10,
   })
 
   if (!data) {

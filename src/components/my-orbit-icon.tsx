3baf33fa1b889220ea8c517339392dda352383fd
Holices Junior <dev.holices@gmail.com>
2025-01-30 11:18:06 +0000

    feat: add project icon

---
+++ src/components/my-orbit-icon.tsx
@@ -0,0 +1,96 @@
+export function MyOrbitIcon() {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>my.orbit icon</title>
+      <path
+        d="M12 0c-.936 0-1.847.107-2.72.31A12.015 12.015 0 00.31 9.28C.107 10.152 0 11.063 0 12c0 6.627 5.372 12 12 12 .935 0 1.846-.107 2.72-.31a12.016 12.016 0 008.97-8.968c.203-.875.31-1.786.31-2.722 0-6.627-5.373-12-12-12zm0 19.515a7.493 7.493 0 01-2.524-.435 7.535 7.535 0 01-4.556-4.555A7.515 7.515 0 1119.515 12 7.515 7.515 0 0112 19.515z"
+        fill="url(#paint0_linear_4018_353)"
+      />
+      <path
+        d="M12 7.157a4.844 4.844 0 100 9.687 4.844 4.844 0 000-9.687zm0 7.772a2.929 2.929 0 110-5.858 2.929 2.929 0 010 5.858z"
+        fill="#00FF38"
+      />
+      <path
+        d="M14.524 4.92a7.514 7.514 0 00-9.604 9.605 7.423 7.423 0 01-2.63-1.702A7.408 7.408 0 01.31 9.28 12.015 12.015 0 019.28.31a7.408 7.408 0 013.543 1.98 7.423 7.423 0 011.701 2.63z"
+        fill="url(#paint1_linear_4018_353)"
+      />
+      <path
+        d="M23.69 14.722a12.016 12.016 0 01-8.97 8.968 7.41 7.41 0 01-3.543-1.98 7.427 7.427 0 01-1.702-2.63 7.515 7.515 0 009.605-9.605c.958.363 1.858.93 2.63 1.702a7.41 7.41 0 011.98 3.545z"
+        fill="url(#paint2_linear_4018_353)"
+      />
+      <path
+        d="M6.064 20.448a2.511 2.511 0 100-5.023 2.511 2.511 0 000 5.023z"
+        fill="url(#paint3_linear_4018_353)"
+      />
+      <path
+        d="M17.958 8.554a2.511 2.511 0 100-5.023 2.511 2.511 0 000 5.023z"
+        fill="url(#paint4_linear_4018_353)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_4018_353"
+          x1={20.37}
+          y1={4.09058}
+          x2={3.88987}
+          y2={19.6632}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#18D814" />
+          <stop offset={0.23} stopColor="#1CC818" />
+          <stop offset={1} stopColor="#00FFE0" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_4018_353"
+          x1={11.2403}
+          y1={2.03429}
+          x2={1.86633}
+          y2={11.3576}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#10C700" />
+          <stop offset={1} stopColor="#00FFA3" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_4018_353"
+          x1={21.9612}
+          y1={12.8134}
+          x2={12.5873}
+          y2={22.1372}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#0ACC1E" />
+          <stop offset={1} stopColor="#00FFD1" />
+        </linearGradient>
+        <linearGradient
+          id="paint3_linear_4018_353"
+          x1={7.89768}
+          y1={16.2204}
+          x2={4.23013}
+          y2={19.6519}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#00FFF0" />
+          <stop offset={1} stopColor="#00FF1A" />
+        </linearGradient>
+        <linearGradient
+          id="paint4_linear_4018_353"
+          x1={19.4842}
+          y1={4.61331}
+          x2={16.9804}
+          y2={6.95642}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#00FFF0" />
+          <stop offset={0.97} stopColor="#00FF38" />
+          <stop offset={1} stopColor="#00FF47" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
